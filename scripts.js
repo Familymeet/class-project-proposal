8837264f14638421fa6f171654d84a24b16a3eb5
Melvin D Hillard III <mhillard@lewisu.edu>
2025-03-23 12:41:17 -0500

    update files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+// Example of a basic function to show a message on the webpage
+function showMessage() {
+    alert("This is a simple message!");
+}
+
+// You can add more interactivity as needed (e.g., search, file preview)
